ae7955c02682b7445344237066bacea9e21e2e15
groy18 <gwendal.roy1@gmail.com>
2019-10-10 17:09:08 +0200

    Initialisation du constructeur de la classe Visiteur

--- src/gsb/modele/Visiteur.java
+++ src/gsb/modele/Visiteur.java
@@ -8,5 +8,25 @@ package gsb.modele;
  */
 public class Visiteur {
 
-		
+	protected String matricule;
+	protected String nom;
+	protected String prenom;
+	protected String login;
+	protected String mdp;
+	protected String adresse;
+	protected String telephone;
+	protected String dateEntrer;
+	protected int prime;
+	
+	public Visiteur(String matricule, String nom, String prenom, String login, String mdp, String adresse, String telephone, String dateEntrer, int prime) {
+		this.matricule = matricule;
+		this.nom = nom;
+		this.prenom= prenom;
+		this.login = login;
+		this.mdp = mdp;
+		this.adresse = adresse;
+		this.telephone = telephone;
+		this.dateEntrer = dateEntrer;
+		this.prime = prime;
+	}	
 }
