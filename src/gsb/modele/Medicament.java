125158063f5e6f975a9a101d8c0940ca8d93cfc4
groy18 <gwendal.roy1@gmail.com>
2019-10-10 16:52:11 +0200

    Initialisation du constructeur de la classe Medicament

--- src/gsb/modele/Medicament.java
+++ src/gsb/modele/Medicament.java
@@ -11,5 +11,21 @@ package gsb.modele;
 
 public class Medicament {
 	
+	protected String depotLegal;
+	protected String nomCommercial;
+	protected String composition;
+	protected String effets;
+	protected String contreIndication;
+	protected int prixEchantillion;
+	
+	public Medicament(String depotLegal, String nomCommercial, String composition, String effets, String contreIndication, int prixEchantillion) {
+		this.depotLegal = depotLegal;
+		this.nomCommercial = nomCommercial;
+		this.composition = composition;
+		this.effets = effets;
+		this.contreIndication = contreIndication;
+		this.prixEchantillion = prixEchantillion;
+	}
+	
 	
 }
