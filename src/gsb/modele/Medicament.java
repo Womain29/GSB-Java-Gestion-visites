9d03a15d70cf5ed5a89ceffbfd6f985ad158c9c4
groy18 <gwendal.roy1@gmail.com>
2019-10-10 16:53:46 +0200

    Réalisation des méthodes set() et get() de la classe Medicament

--- src/gsb/modele/Medicament.java
+++ src/gsb/modele/Medicament.java
@@ -27,5 +27,81 @@ public class Medicament {
 		this.prixEchantillion = prixEchantillion;
 	}
 	
+	/**
+	 * @return Renvoie depotLegal.
+	 */
+	public String getDepotLegal() {
+		return depotLegal;
+	}
+	/**
+	 * @param depotLegal à définir.
+	 */
+	public void setDepotLegal(String depotLegal) {
+		this.depotLegal = depotLegal;
+	}
 	
+	/**
+	 * @return Renvoie nomCommercial.
+	 */
+	public String getNomCommercial() {
+		return nomCommercial;
+	}
+	/**
+	 * @param nomCommercial à définir.
+	 */
+	public void setNomCommercial(String nomCommercial) {
+		this.nomCommercial = nomCommercial;
+	}
+	
+	/**
+	 * @return Renvoie composition.
+	 */
+	public String getComposition() {
+		return composition;
+	}
+	/**
+	 * @param composition à définir.
+	 */
+	public void setComposition(String composition) {
+		this.composition = composition;
+	}
+	
+	/**
+	 * @return Renvoie effets.
+	 */
+	public String getEffets() {
+		return effets;
+	}
+	/**
+	 * @param effets à définir.
+	 */
+	public void setEffets(String effets) {
+		this.effets = effets;
+	}
+	
+	/**
+	 * @return Renvoie contreIndication.
+	 */
+	public String getContreIndication() {
+		return contreIndication;
+	}
+	/**
+	 * @param contreIndication à définir.
+	 */
+	public void setContreIndication(String contreIndication) {
+		this.contreIndication = contreIndication;
+	}
+	
+	/**
+	 * @return Renvoie prixEchantillion.
+	 */
+	public int getPrixEchantillion() {
+		return prixEchantillion;
+	}
+	/**
+	 * @param prixEchantillion à définir.
+	 */
+	public void setPrixEchantillion(int prixEchantillion) {
+		this.prixEchantillion = prixEchantillion;
+	}
 }
