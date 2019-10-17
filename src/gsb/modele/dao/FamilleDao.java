27da5f3289506c0473f6d70905912b6dc3c80e50
groy18 <gwendal.roy1@gmail.com>
2019-10-17 15:54:18 +0200

    Création de la méthode rechercher de FamilleDao

--- src/gsb/modele/dao/FamilleDao.java
+++ src/gsb/modele/dao/FamilleDao.java
@@ -1,6 +1,9 @@
 /*Créé le 17/10/2019*/
 package gsb.modele.dao;
 
+import java.sql.ResultSet;
+
+import gsb.modele.Famille;
 
 /**
  * @author Gwendal
@@ -10,4 +13,19 @@ package gsb.modele.dao;
 
 public class FamilleDao {
 	
+	public static Famille rechercher(String codeFamille){
+		Famille uneFamille=null;
+		ResultSet reqSelection = ConnexionMySql.execReqSelection("select * from FAMILLE where CODEFAMILLE='"+codeFamille+"'");
+		try {
+			if (reqSelection.next()) {
+				uneFamille = new Famille(reqSelection.getString(1), reqSelection.getString(2));	
+			};
+			}
+		catch(Exception e) {
+			System.out.println("erreur reqSelection.next() pour la requ?te - select * from FAMILLE where CODEFAMILLE='"+codeFamille+"'");
+			e.printStackTrace();
+			}
+		ConnexionMySql.fermerConnexionBd();
+		return uneFamille;
+	}
 }
