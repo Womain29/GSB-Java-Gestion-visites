b8a2ee70f360d2b02bd11c5f93a5268ac7858a43
Womain <romain.perennes@gmail.com>
2019-10-17 15:09:33 +0200

    Création set et get

--- src/gsb/modele/Unite.java
+++ src/gsb/modele/Unite.java
@@ -9,4 +9,20 @@ public class Unite {
         this.codeUnite = codeUnite;
         this.nomUnite = nomUnite;
     }
+
+    public String getCodeUnite() {
+        return codeUnite;
+    }
+
+    public void setCodeUnite(String codeUnite) {
+        this.codeUnite = codeUnite;
+    }
+
+    public String getNomUnite() {
+        return nomUnite;
+    }
+
+    public void setNomUnite(String nomUnite) {
+        this.nomUnite = nomUnite;
+    }
 }
