541b840dd70efc41ed65fb99089b1af10c8dd01a
groy18 <gwendal.roy1@gmail.com>
2019-10-17 16:44:15 +0200

    Ajout des méthodes set() et get() de la classe Stock

--- src/gsb/modele/Stock.java
+++ src/gsb/modele/Stock.java
@@ -18,4 +18,45 @@ public class Stock {
 		this.matricule = matricule;
 		this.qteStock = qteStock;
 	}
+
+	/**
+	 * @return Renvoie depotLegal.
+	 */
+	public String getDepotLegal() {
+		return depotLegal;
+	}
+	/**
+	 * @param depotLegal à définir.
+	 */
+	public void setDepotLegal(String depotLegal) {
+		this.depotLegal = depotLegal;
+	}
+	
+	/**
+	 * @return Renvoie matricule.
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+	/**
+	 * @param matricule à définir.
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	
+	/**
+	 * @return Renvoie qteStock.
+	 */
+	public int getQteStock() {
+		return qteStock;
+	}
+	/**
+	 * @param qteStock à définir.
+	 */
+	public void setQteStock(int qteStock) {
+		this.qteStock = qteStock;
+	}
+	
+	
 }
