f255834aca7730a2eec4e470352f8a054661cbfc
groy18 <gwendal.roy1@gmail.com>
2019-10-17 14:11:13 +0200

    Création de la classe MedicamentDao

---
+++ src/gsb/modele/dao/MedicamentDao.java
@@ -0,0 +1,18 @@
+/*Créé le 15/10/2019 */
+package gsb.modele.dao;
+
+import java.sql.ResultSet;
+
+import gsb.modele.Medicament;
+
+/**
+ * @author Gwendal
+ * 10/10/2019
+ * 
+ */
+
+public class MedicamentDao {
+	
+	
+	
+}
