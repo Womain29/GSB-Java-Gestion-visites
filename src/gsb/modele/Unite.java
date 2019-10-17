c252174ea5b910f86d3ef55cbf1b3c86dd2786c9
Womain <romain.perennes@gmail.com>
2019-10-17 15:08:58 +0200

    Création contructeur

--- src/gsb/modele/Unite.java
+++ src/gsb/modele/Unite.java
@@ -5,6 +5,8 @@ public class Unite {
     protected String codeUnite;
     protected String nomUnite;
 
-
-
+    public Unite(String codeUnite, String nomUnite) {
+        this.codeUnite = codeUnite;
+        this.nomUnite = nomUnite;
+    }
 }
