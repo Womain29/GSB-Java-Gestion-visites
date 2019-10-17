67340daa2383108774520fa54955619aa7a0f033
groy18 <gwendal.roy1@gmail.com>
2019-10-17 17:18:18 +0200

    Création de la méthode rechercherMedicament de MedicamentService

--- src/gsb/service/MedicamentService.java
+++ src/gsb/service/MedicamentService.java
@@ -1,5 +1,27 @@
+/*Créé le 17/10/2019 */
 package gsb.service;
 
-public class MedicamentService {
+import gsb.modele.Medicament;
+import gsb.modele.dao.MedicamentDao;
 
+/**
+ * @author Gwendal
+ * 17/10/2019
+ * 
+ */
+public class MedicamentService {
+	
+	public static Medicament rechercherMedicament(String unDepotLegal){
+		Medicament unMedicament = null;
+		try{
+		if (unDepotLegal==null) {
+            throw new Exception("Donnée obligatoire : depotLegal");
+        }
+		unMedicament = MedicamentDao.rechercher(unDepotLegal);
+		}
+		catch(Exception e){
+			System.out.println( e.getMessage());
+		}
+		return unMedicament;
+	}
 }
