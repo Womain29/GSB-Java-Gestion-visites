36b99b1be7e55d498a3870157f2245486ba0b1ce
Womain <romain.perennes@gmail.com>
2019-10-26 15:52:20 +0200

    Création set et get

--- src/gsb/modele/Visite.java
+++ src/gsb/modele/Visite.java
@@ -31,4 +31,84 @@ public class Visite {
         this.unVisiteur = unVisiteur;
         this.unMedecin = unMedecin;
     }
+
+    /**
+     *
+     * @return la référence de la visite
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    /**
+     *
+     * @param reference chaine de caracteres
+     */
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     *
+     * @return la date de la visite
+     */
+    public String getDateVisite() {
+        return dateVisite;
+    }
+
+    /**
+     *
+     * @param dateVisite chaine de caracteres
+     */
+    public void setDateVisite(String dateVisite) {
+        this.dateVisite = dateVisite;
+    }
+
+    /**
+     *
+     * @return commentaire sur la visite
+     */
+    public String getUnCommentaire() {
+        return unCommentaire;
+    }
+
+    /**
+     *
+     * @param unCommentaire chaine de caractere
+     */
+    public void setUnCommentaire(String unCommentaire) {
+        this.unCommentaire = unCommentaire;
+    }
+
+    /**
+     *
+     * @return un objet Visiteur
+     */
+    public Visiteur getUnVisiteur() {
+        return unVisiteur;
+    }
+
+    /**
+     *
+     * @param unVisiteur objet Visisteur
+     */
+    public void setUnVisiteur(Visiteur unVisiteur) {
+        this.unVisiteur = unVisiteur;
+    }
+
+    /**
+     *
+     * @return un objet Medecin
+     */
+    public Medecin getUnMedecin() {
+        return unMedecin;
+    }
+
+    /**
+     *
+     * @param unMedecin objet Medecin
+     */
+    public void setUnMedecin(Medecin unMedecin) {
+        this.unMedecin = unMedecin;
+    }
 }
