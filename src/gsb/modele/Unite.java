12e2b027da34f3248584b911f5e1038c7b0ac383
Womain <romain.perennes@gmail.com>
2019-10-26 12:34:46 +0200

    Ajout des commentaires JavaDoc

--- src/gsb/modele/Unite.java
+++ src/gsb/modele/Unite.java
@@ -1,27 +1,55 @@
 package gsb.modele;
 
+/**
+ * Classe Unite
+ * Les visiteurs font partis d'une unite
+ *
+ * @author womain
+ * 26/10/2019
+ */
+
 public class Unite {
 
+    //Attributs
     protected String codeUnite;
     protected String nomUnite;
 
+    //Constructeur
     public Unite(String codeUnite, String nomUnite) {
         this.codeUnite = codeUnite;
         this.nomUnite = nomUnite;
     }
 
+    //Set et get
+
+    /**
+     *
+     * @return le code de l'unite
+     */
     public String getCodeUnite() {
         return codeUnite;
     }
 
+    /**
+     *
+     * @param codeUnite chaine de caracteres
+     */
     public void setCodeUnite(String codeUnite) {
         this.codeUnite = codeUnite;
     }
 
+    /**
+     *
+     * @return le nom de l'unite
+     */
     public String getNomUnite() {
         return nomUnite;
     }
 
+    /**
+     *
+     * @param nomUnite chaine de caracteres
+     */
     public void setNomUnite(String nomUnite) {
         this.nomUnite = nomUnite;
     }
