a0dc2d3a4804844919022d6d22d9067809f57864
Womain <romain.perennes@gmail.com>
2019-10-26 15:37:57 +0200

    Création du constructeur

--- src/gsb/modele/Visite.java
+++ src/gsb/modele/Visite.java
@@ -14,4 +14,21 @@ public class Visite {
     protected String unCommentaire;
     protected Visiteur unVisiteur;
     protected Medecin unMedecin;
+
+    /**
+     *
+     * @param reference code de la visite
+     * @param dateVisite date de la visite
+     * @param unCommentaire commenaitaire concernant la visite
+     * @param unVisiteur visiteur qui effectue la visite
+     * @param unMedecin medecin chez qui la visite est effectuée
+     */
+
+    public Visite(String reference, String dateVisite, String unCommentaire, Visiteur unVisiteur, Medecin unMedecin) {
+        this.reference = reference;
+        this.dateVisite = dateVisite;
+        this.unCommentaire = unCommentaire;
+        this.unVisiteur = unVisiteur;
+        this.unMedecin = unMedecin;
+    }
 }
