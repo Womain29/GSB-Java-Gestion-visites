b4cb2a17186bf11baece9b9519bfef57e96c906d
Womain <romain.perennes@gmail.com>
2019-10-27 14:36:31 +0100

    Création classe VisiteServiceTest

---
+++ src/gsb/tests/VisiteServiceTest.java
@@ -0,0 +1,28 @@
+package gsb.tests;
+
+import gsb.service.VisiteService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VisiteServiceTest {
+
+    private VisiteService uneVisiteService;
+
+    @BeforeEach
+    void setUp() {
+        uneVisiteService = new VisiteService();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void rechercherVisite() {
+    }
+
+    @Test
+    void creerVisite() {
+    }
+}
