a201f1e7ae8c35415c9411c4619b93c1b6d8012f
Womain <romain.perennes@gmail.com>
2019-10-27 21:21:24 +0100

    Ajout méthode de test creerVisiteCodeMedKO

--- src/gsb/tests/VisiteServiceTest.java
+++ src/gsb/tests/VisiteServiceTest.java
@@ -130,4 +130,10 @@ class VisiteServiceTest {
         System.out.println("--------------------------- creerVisiteCodeMed5Carac ---------------------------------");
         Assertions.assertEquals(0, uneVisiteService.creerVisite("test", "06/06/1996", "", "a131", "AAAAA"), "Résultat 0 car le code medecin trop long");
     }
+
+    @Test
+    void creerVisiteCodeMedKO() {
+        System.out.println("--------------------------- creerVisiteCodeMedKO ---------------------------------");
+        Assertions.assertEquals(0, uneVisiteService.creerVisite("test", "06/06/1996", "", "a131", "BBBB"), "Résultat 0 car le code medecin non existant");
+    }
 }
