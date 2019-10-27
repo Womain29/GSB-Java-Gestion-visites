b74778c8b381fa0672d77f58938659e25a8eadc2
Womain <romain.perennes@gmail.com>
2019-10-27 14:57:49 +0100

    Création des tests pour rechercherVisite

--- src/gsb/tests/VisiteServiceTest.java
+++ src/gsb/tests/VisiteServiceTest.java
@@ -1,7 +1,9 @@
 package gsb.tests;
 
+import gsb.modele.Visite;
 import gsb.service.VisiteService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +21,34 @@ class VisiteServiceTest {
     }
 
     @Test
-    void rechercherVisite() {
+    void rechercherVisiteReferenceVide() {
+        System.out.println("--------------------------- rechercherVisiteReferenceVide ---------------------------------");
+        Assertions.assertNull(uneVisiteService.rechercherVisite(null), "Résultat null car référence vide");
+    }
+
+    @Test
+    void rechercherVisiteReference6caracteres() {
+        System.out.println("--------------------------- rechercherVisiteReference6caracteres ---------------------------------");
+        Assertions.assertNull(uneVisiteService.rechercherVisite("AAAAAA"), "Résultat null car référence trop longue");
+    }
+
+    @Test
+    void rechercherVisiteReferenceKO() {
+        System.out.println("--------------------------- rechercherVisiteReferenceKO ---------------------------------");
+        Assertions.assertNull(uneVisiteService.rechercherVisite("aaaaa"), "Résultat null car pas de visite correspondante");
+    }
+
+    @Test
+    void rechercherVisiteReferenceOK() {
+        System.out.println("--------------------------- rechercherVisiteReferenceOK ---------------------------------");
+        Assertions.assertNotNull(uneVisiteService.rechercherVisite("v0001"), "Résultat objet car une visite correspondante");
+
+        Visite uneVisite = uneVisiteService.rechercherVisite("v0001");
+        System.out.println("Référence : " + uneVisite.getReference());
+        System.out.println("Date : " + uneVisite.getDateVisite());
+        System.out.println("Commentaire : " + uneVisite.getUnCommentaire());
+        System.out.println("Matricule Visiteur : " + uneVisite.getUnVisiteur().getMatricule());
+        System.out.println("Code Médecin : " + uneVisite.getUnMedecin().getCodeMed());
     }
 
     @Test
