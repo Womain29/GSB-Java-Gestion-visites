d45040b186270683f9b37282e874b674c24fccd7
Womain <romain.perennes@gmail.com>
2019-10-28 15:18:54 +0100

    Création méthode test rechercherMedecinCodeNull

--- src/gsb/tests/MedecinServiceTest.java
+++ src/gsb/tests/MedecinServiceTest.java
@@ -1,38 +1,34 @@
-/*
- * Cr?? le 23 f?vr. 2015
- *
- * TODO Pour changer le mod?le de ce fichier g?n?r?, allez ? :
- * Fen?tre - Pr?f?rences - Java - Style de code - Mod?les de code
- */
 package gsb.tests;
 
-import gsb.modele.Medecin;
 import gsb.service.MedecinService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
- * @author Isabelle
- * 23 f?vr. 2015
- * TODO Pour changer le mod?le de ce commentaire de type g?n?r?, allez ? :
- * Fen?tre - Pr?f?rences - Java - Style de code - Mod?les de code
+ * Classe de test de MedecinService
+ *
+ * @author womain
+ * 28/10/2019
  */
-public class MedecinServiceTest {
+class MedecinServiceTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Medecin unMedecin = MedecinService.rechercherMedecin("m002");
-		System.out.println(unMedecin.getNom());
-		System.out.println(unMedecin.getPrenom());
-		System.out.println(unMedecin.getAdresse());
-		System.out.println(unMedecin.getLaLocalite().getCodePostal());
-		System.out.println(unMedecin.getLaLocalite().getVille());
-		System.out.println(unMedecin.getTelephone());
-		System.out.println(unMedecin.getPotentiel());
-		System.out.println(unMedecin.getSpecialite());
-		
-		
+    private MedecinService unMedecinService;
 
-	}
+    @BeforeEach
+    void setUp() {
+        unMedecinService = new MedecinService();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void rechercherMedecinCodeNull() {
+        System.out.println("--------------------------------- rechercherMedecinCodeNull ------------------------------");
+        Assertions.assertNull(unMedecinService.rechercherMedecin(null), "Résultat null car code null");
+    }
 
 }
