8316f23a463f97fe88907258fd916218d872ba8a
Womain <romain.perennes@gmail.com>
2019-10-28 21:34:22 +0100

    Création classe JIFVisiteAjout

---
+++ src/gsb/vue/JIFVisiteAjout.java
@@ -0,0 +1,20 @@
+package gsb.vue;
+
+import javax.swing.*;
+
+/**
+ * @author womain
+ * 28/10/2019
+ */
+public class JIFVisiteAjout extends JInternalFrame {
+
+    protected JPanel p;
+    protected JPanel pTexte;
+    protected JPanel pBoutons;
+
+    protected JLabel JLReference;
+    protected JLabel JLDate;
+    protected JLabel JLCommentaire;
+    protected JLabel JLVisiteur;
+    protected JLabel JLMedecin;
+}
