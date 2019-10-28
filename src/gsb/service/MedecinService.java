22bef73357a09ff07120454ab696734a4b3a97fb
Womain <romain.perennes@gmail.com>
2019-10-28 15:04:03 +0100

    Ajout JavaDoc

--- src/gsb/service/MedecinService.java
+++ src/gsb/service/MedecinService.java
@@ -12,24 +12,32 @@ import gsb.modele.dao.MedecinDao;
 /**
  * @author Isabelle
  * 23 f?vr. 2015
- * TODO Pour changer le mod?le de ce commentaire de type g?n?r?, allez ? :
- * Fen?tre - Pr?f?rences - Java - Style de code - Mod?les de code
+ *
+ * Modifié le 28/10/2019
  */
 public class MedecinService {
-	
+
+	/**
+	 *
+	 * @param unCodeMedecin chaine de caracteres
+	 * @return un objet Medecin ou null
+	 */
 	public static Medecin rechercherMedecin(String unCodeMedecin){
 		Medecin unMedecin = null;
-		try{
-		if (unCodeMedecin==null) {
-            throw new Exception("Donn?e obligatoire : code");
-        }
-		if (unCodeMedecin.length() > 4) {
-			throw new Exception("Le code du médecin ne peut pas dépasser 4 caractères");
-		}
-		if(MedecinDao.rechercher(unCodeMedecin) == null) {
-			throw new Exception("Il n'y a pas de médecin correspondant à ce code");
-		}
-		unMedecin = MedecinDao.rechercher(unCodeMedecin);
+		try {
+			//Code du médecin obligatoire
+			if (unCodeMedecin==null) {
+				throw new Exception("Le code du médecin est obligatoire");
+			}
+			//4 caractères maximum pour le code du médecin
+			if (unCodeMedecin.length() > 4) {
+				throw new Exception("Le code du médecin ne peut pas dépasser 4 caractères");
+			}
+			//Le code doit correspondre à un médecin
+			if(MedecinDao.rechercher(unCodeMedecin) == null) {
+				throw new Exception("Il n'y a pas de médecin correspondant à ce code");
+			}
+			unMedecin = MedecinDao.rechercher(unCodeMedecin);
 		}
 		catch(Exception e){
 			System.out.println( e.getMessage());
