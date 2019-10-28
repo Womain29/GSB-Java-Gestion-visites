fbae08e7f19569f6765fc3edb1c105bd9f74183c
Womain <romain.perennes@gmail.com>
2019-10-28 15:32:08 +0100

    Création méthode test rechercherMedecinCodeOk

--- src/gsb/tests/MedecinServiceTest.java
+++ src/gsb/tests/MedecinServiceTest.java
@@ -1,5 +1,6 @@
 package gsb.tests;
 
+import gsb.modele.Medecin;
 import gsb.service.MedecinService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -41,6 +42,22 @@ class MedecinServiceTest {
     void rechercherMedecinCodeKo() {
         System.out.println("--------------------------------- rechercherMedecinCodeKo ------------------------------");
         Assertions.assertNull(unMedecinService.rechercherMedecin("BBBB"), "Résultat null car code non existant dans la BDD");
+    }
+
+    @Test
+    void rechercherMedecinCodeOk() {
+        System.out.println("--------------------------------- rechercherMedecinCodeOk ------------------------------");
+        Assertions.assertNotNull(unMedecinService.rechercherMedecin("m001"), "Résultat non null car code ok");
+
+        Medecin unMedecin = unMedecinService.rechercherMedecin("m001");
+        System.out.println("Code Médecin : " + unMedecin.getCodeMed());
+        System.out.println("Nom : " + unMedecin.getNom());
+        System.out.println("Prénom : " + unMedecin.getPrenom());
+        System.out.println("Adresse : " + unMedecin.getAdresse());
+        System.out.println("Ville : " + unMedecin.getLaLocalite().getVille());
+        System.out.println("Téléphone : " + unMedecin.getTelephone());
+        System.out.println("Potentiel : " + unMedecin.getPotentiel());
+        System.out.println("Spécialité : " + unMedecin.getSpecialite());
     }
 
 }
