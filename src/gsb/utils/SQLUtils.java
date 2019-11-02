e93675b3195821b45fc7ab1c816c2218806e371f
Womain <romain.perennes@gmail.com>
2019-11-02 17:50:08 +0100

    Correction erreur sur l'année

--- src/gsb/utils/SQLUtils.java
+++ src/gsb/utils/SQLUtils.java
@@ -3,7 +3,7 @@ package gsb.utils;
 public class SQLUtils {
 
     public static String dateFormatSql(String uneDate) {
-        String annee = uneDate.substring(6,9);
+        String annee = uneDate.substring(6,10);
         String mois = uneDate.substring(3,5);
         String jour = uneDate.substring(0,2);
 
