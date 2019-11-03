c736356579a9756008f0a3122d2fab36d5a7a623
Womain <romain.perennes@gmail.com>
2019-11-03 21:22:51 +0100

    Création fenêtre JIFVisiteAjout

--- src/gsb/vue/JIFVisiteAjout.java
+++ src/gsb/vue/JIFVisiteAjout.java
@@ -1,20 +1,42 @@
 package gsb.vue;
 
+import gsb.service.VisiteService;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * @author womain
- * 28/10/2019
- */
-public class JIFVisiteAjout extends JInternalFrame {
+public class JIFVisiteAjout extends JIFVisite implements ActionListener {
 
-    protected JPanel p;
-    protected JPanel pTexte;
-    protected JPanel pBoutons;
+    private JButton JBValider;
+    private JButton JBAnnuler;
 
-    protected JLabel JLReference;
-    protected JLabel JLDate;
-    protected JLabel JLCommentaire;
-    protected JLabel JLVisiteur;
-    protected JLabel JLMedecin;
+    private VisiteService uneVisiteService = new VisiteService();
+
+    public JIFVisiteAjout() {
+        super();
+
+        JBValider = new JButton("Valider");
+        JBValider.addActionListener(this);
+        JBAnnuler = new JButton("Annuler");
+        JBAnnuler.addActionListener(this);
+
+        pBoutons.add(JBValider);
+        pBoutons.add(JBAnnuler);
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setTitle("Ajout d'une visite");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if(source == JBValider) {
+            uneVisiteService.creerVisite(JTReference.getText().toString(), JTDate.getText().toString(), JTCommentaire.getText().toString(), JTVisiteur.getText().toString(), JTMedecin.getText().toString());
+            this.videTexte();
+        }
+        if(source == JBAnnuler) {
+            this.videTexte();
+        }
+    }
 }
