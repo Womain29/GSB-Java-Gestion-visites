63e127e2f3dacde22326c7b2c1a97b4c49ae56a2
Womain <romain.perennes@gmail.com>
2019-11-05 17:27:18 +0100

    Ajout de controle de saisie avec affichage des execeptions

--- src/gsb/vue/JIFVisiteListe.java
+++ src/gsb/vue/JIFVisiteListe.java
@@ -2,7 +2,9 @@ package gsb.vue;
 
 import gsb.modele.Visite;
 import gsb.modele.dao.VisiteDao;
+import gsb.modele.dao.VisiteurDao;
 import gsb.service.VisiteService;
+import gsb.utils.ValidationUtils;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -25,6 +27,7 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
     protected JPanel pRecherche;
     protected JPanel pRecherche1;
     protected JPanel pRecherche2;
+    protected JPanel pRecherche3;
     protected JPanel pDetail;
 
     //Déclaration des composants du tableau
@@ -40,6 +43,7 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
     protected JLabel JLDate;
     protected JLabel JLMatri;
     protected JLabel JLRef;
+    protected JLabel JLErreurRecherche;
 
     //Déclaration des boutons
     protected JButton JBRechercher;
@@ -52,25 +56,38 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
 
     private VisiteService uneVisiteService = new VisiteService();
 
+    private JOptionPane erreurRecherche;
+
     public JIFVisiteListe() {
         super();
 
         //Instanciation des panneaux avec ou sans grille
         p = new JPanel();
-        pRecherche = new JPanel(new GridLayout(1,2));
+        p.setSize(1100, 750);
+        pRecherche = new JPanel(new GridLayout(1,3));
+        pRecherche.setMinimumSize(new Dimension(1100, 250));
         pRecherche1 = new JPanel(new GridLayout(2,2,10,5));
+        pRecherche1.setMinimumSize(new Dimension(300,250));
         pRecherche2 = new JPanel();
+        pRecherche2.setMinimumSize(new Dimension(200, 250));
+        pRecherche3 = new JPanel();
+        pRecherche3.setMinimumSize(new Dimension(600,250));
         pDetail = new JPanel(new GridLayout(1,3,5,5));
+        pDetail.setSize(1100,100);
 
         //Instanciation des JTexField
-        JTDateVisite = new JTextField();
-        JTMatriVisite = new JTextField();
-        JTRefVisite = new JTextField();
+        JTDateVisite = new JTextField(10);
+        JTDateVisite.addMouseListener(this);
+        JTMatriVisite = new JTextField(10);
+        JTMatriVisite.addMouseListener(this);
+        JTRefVisite = new JTextField(10);
 
         //Instanciation des labels
         JLDate = new JLabel("Date");
         JLMatri = new JLabel("Matricule");
         JLRef = new JLabel("Référence");
+        JLErreurRecherche = new JLabel();
+        JLErreurRecherche.setForeground(new Color(255,0,0));
 
         //Instanciation des événements
         JBRechercher = new JButton("Rechercher");
@@ -84,10 +101,12 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
         pRecherche1.add(JLMatri);
         pRecherche1.add(JTMatriVisite);
         pRecherche2.add(JBRechercher);
+        pRecherche3.add(JLErreurRecherche);
 
         //Ajout des sous-panneaux sur le panneau principal de recherche
         pRecherche.add(pRecherche1);
         pRecherche.add(pRecherche2);
+        pRecherche.add(pRecherche3);
 
         //Ajout des éléments sur le panneau de détail
         pDetail.add(JLRef);
@@ -100,7 +119,7 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
         table = new JTable(model); //Ajout des données dans le tableau
         table.addMouseListener(this); //Ecouteur de la souris
         scrollPane = new JScrollPane(table); //Ajout du tableau avec barre de défilement
-        scrollPane.setPreferredSize(new Dimension(400, 200));
+        scrollPane.setPreferredSize(new Dimension(1100, 400));
 
         //Ajouts au panbeau principal
         p.add(pRecherche);
@@ -121,11 +140,35 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
             model.setRowCount(0); //Vide les lignes du tableau
 
             ArrayList<Visite> colVisiteDateRef;
-            colVisiteDateRef = uneVisiteService.rechercheVisiteDateMat(JTDateVisite.getText().toString(), JTMatriVisite.getText().toString());
+            String date = JTDateVisite.getText().toString();
+            String matricule = JTMatriVisite.getText().toString();
 
-            for(Visite uneVisite : colVisiteDateRef) {
-                String[] visite = {uneVisite.getReference(), uneVisite.getDateVisite(), uneVisite.getUnCommentaire(), uneVisite.getUnVisiteur().getMatricule(), uneVisite.getUnMedecin().getCodeMed()};
-                model.addRow(visite); //Ajoute une visite dans le tableau
+            try {
+                //Les champs ne peuvent pas être null
+                if (date.equals("") || matricule.equals("")) {
+                    throw new Exception("Tous les champs sont obligatoires");
+                }
+                //La date doit être au format dd/MM/yyyy
+                if (!ValidationUtils.isDateValide(date)) {
+                    throw new Exception("La date doit être au format dd/MM/yyyy");
+                }
+                //Une visite correspondante à la référence doit exister
+                if (VisiteurDao.rechercher(matricule) == null) {
+                    throw new Exception("Le visiteur correspondant à ce matricule n'existe pas");
+                }
+                //La référence ne peut pas dépasser 4 caracteres
+                if (matricule.length() > 4) {
+                    throw new Exception("Le matricule ne peut pas dépasser 5 caractères");
+                }
+                colVisiteDateRef = uneVisiteService.rechercheVisiteDateMat(date, matricule);
+                for(Visite uneVisite : colVisiteDateRef) {
+                    String[] visite = {uneVisite.getReference(), uneVisite.getDateVisite(), uneVisite.getUnCommentaire(), uneVisite.getUnVisiteur().getMatricule(), uneVisite.getUnMedecin().getCodeMed()};
+                    model.addRow(visite); //Ajoute une visite dans le tableau
+                }
+            }
+            catch (Exception erreur) {
+                System.out.println(erreur.getMessage());
+                JLErreurRecherche.setText(erreur.getMessage());
             }
         }
         //Ouvre la fenêtre du récapitulatif
@@ -137,9 +180,16 @@ public class JIFVisiteListe extends JInternalFrame implements ActionListener, Mo
     //Ecouteur au clic de la souris
     @Override
     public void mouseClicked(MouseEvent e) {
-        int ligne = table.getSelectedRow(); //Récupère la ligne du tableau
-        String ref = table.getModel().getValueAt(ligne, 0).toString(); //Récupère la référence de la ligne
-        JTRefVisite.setText(ref); //Initialise le JTextfield sur la référence
+        Object mouse = e.getSource();
+        if(mouse == table) {
+            int ligne = table.getSelectedRow(); //Récupère la ligne du tableau
+            String ref = table.getModel().getValueAt(ligne, 0).toString(); //Récupère la référence de la ligne
+            JTRefVisite.setText(ref); //Initialise le JTextfield sur la référence
+        }
+        if(mouse == JTDateVisite || mouse == JTMatriVisite) {
+            JLErreurRecherche.setText("");
+        }
+
     }
 
     @Override
