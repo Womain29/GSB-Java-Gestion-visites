be02e4e8ef6e67e81665d5f24d9dc5000c8dad39
Womain <romain.perennes@gmail.com>
2019-11-07 14:46:01 +0100

    Ajout commentaires JavaDoc

--- src/gsb/utils/SQLUtils.java
+++ src/gsb/utils/SQLUtils.java
@@ -1,6 +1,20 @@
 package gsb.utils;
 
+/**
+ * @author womain
+ *
+ * Classe utilitaires pour Mysql
+ *
+ * 02/11/2019
+ */
+
 public class SQLUtils {
+
+    /**
+     *
+     * @param uneDate chaine de caracteres de type 00/00/0000
+     * @return une chaine de caracteres au format date 0000-00-00
+     */
 
     public static String dateFormatSql(String uneDate) {
         String annee = uneDate.substring(6,10);
