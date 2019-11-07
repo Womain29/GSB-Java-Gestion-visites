59aed146a1b7b709f3cf2b29d19a264984c4e7cc
Womain <romain.perennes@gmail.com>
2019-11-07 15:04:32 +0100

    Ajout commentaires

--- src/gsb/vue/JIFVisite.java
+++ src/gsb/vue/JIFVisite.java
@@ -7,20 +7,26 @@ import java.awt.*;
 
 /**
  * @author womain
+ *
+ * Fenêtre générale d'une visite
+ *
  * 28/10/2019
  */
 public class JIFVisite extends JInternalFrame {
 
+    //Déclaration des JPanel
     protected JPanel p;
     protected JPanel pTexte;
     protected JPanel pBoutons;
 
+    //Déclaration des JLabel
     protected JLabel JLReference;
     protected JLabel JLDate;
     protected JLabel JLCommentaire;
     protected JLabel JLVisiteur;
     protected JLabel JLMedecin;
 
+    //Déclaration des JTextField
     protected JTextField JTReference;
     protected JTextField JTDate;
     protected JTextField JTCommentaire;
@@ -28,22 +34,27 @@ public class JIFVisite extends JInternalFrame {
     protected JTextField JTMedecin;
 
     public JIFVisite() {
+
+        //Instanciation des panneaux
         p = new JPanel();
         pTexte = new JPanel(new GridLayout(5,2,5,5));
         pBoutons = new JPanel();
 
+        //Instanciation des JLabel
         JLReference = new JLabel("Référence");
         JLDate = new JLabel("Date");
         JLCommentaire = new JLabel("Commentaire");
         JLVisiteur = new JLabel("Visiteur");
         JLMedecin = new JLabel("Médecin");
 
+        //Instanciation des JTextField
         JTReference = new JTextField(15);
         JTDate = new JTextField(15);
         JTCommentaire = new JTextField(15);
         JTVisiteur = new JTextField(15);
         JTMedecin = new JTextField(15);
 
+        //Ajout au panneau des textes
         pTexte.add(JLReference);
         pTexte.add(JTReference);
         pTexte.add(JLDate);
@@ -55,13 +66,21 @@ public class JIFVisite extends JInternalFrame {
         pTexte.add(JLMedecin);
         pTexte.add(JTMedecin);
 
+        //Ajout au panneau principal
         p.add(pTexte);
         p.add(pBoutons);
 
+        //Ajout du panneau principal à la fenêtre
         Container contentPane = getContentPane();
         contentPane.add(p);
     }
 
+    /**
+     *
+     * @param uneVisite objet Visite
+     *
+     * Permet de remplir les JTextField avec les attributs d'une visite
+     */
     public void remplirText(Visite uneVisite) {
         JTReference.setText(uneVisite.getReference());
         JTDate.setText(uneVisite.getDateVisite());
@@ -70,6 +89,7 @@ public class JIFVisite extends JInternalFrame {
         JTMedecin.setText(uneVisite.getUnMedecin().getCodeMed());
     }
 
+    //Permet de vider les JTextField
     public void videTexte() {
         JTReference.setText("");
         JTDate.setText("");
