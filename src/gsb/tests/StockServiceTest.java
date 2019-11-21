48f7f21e2ba066351c95d4ed20cce7d2a3fc76be
groy18 <gwendal.roy1@gmail.com>
2019-11-21 14:55:57 +0100

    Test unitaire ajoutStockAllNull

--- src/gsb/tests/StockServiceTest.java
+++ src/gsb/tests/StockServiceTest.java
@@ -5,6 +5,8 @@
 package gsb.tests;
 import gsb.modele.*;
 import gsb.service.StockService;
+import gsb.service.VisiteService;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,5 +17,25 @@ import org.junit.jupiter.api.Test;
  */
 
 public class StockServiceTest {
- 
+	 
+	 private StockService unStockService;
+	 private Stock unStock;
+	 
+	 @BeforeEach
+	    void setUp() {
+	        unStockService = new StockService();
+	    }
+
+	    @AfterEach
+	    void tearDown() {
+	        
+	    }
+	    
+	    @Test
+	    void ajoutStockAllNull() {
+	        System.out.println("--------------------------- ajoutStockAllNull ---------------------------------");
+	        Assertions.assertEquals(0, unStockService.ajoutStock(null, null, 0), "Résultat 0 car paramètres null");
+	    }
+	    
+	    
 }
