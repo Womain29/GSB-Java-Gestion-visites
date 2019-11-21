801f01a7bcaa438e42951d51e209f6cdedc66c1b
groy18 <gwendal.roy1@gmail.com>
2019-11-21 14:47:21 +0100

    Création de la classe StockServiceTest

--- src/gsb/tests/StockServiceTest.java
+++ src/gsb/tests/StockServiceTest.java
@@ -3,10 +3,17 @@
 */
 
 package gsb.tests;
+import gsb.modele.*;
+import gsb.service.StockService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 /*
  * @author Gwendal
  * 21/11/2019
  */
+
 public class StockServiceTest {
 
 }
