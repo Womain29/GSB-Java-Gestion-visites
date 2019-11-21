7f222b70dd664dc2e39faa171710f16e7cb52314
groy18 <gwendal.roy1@gmail.com>
2019-11-21 15:00:54 +0100

    Test unitaire ajoutStockQteStockNull

--- src/gsb/tests/StockServiceTest.java
+++ src/gsb/tests/StockServiceTest.java
@@ -37,5 +37,21 @@ public class StockServiceTest {
 	        Assertions.assertEquals(0, unStockService.ajoutStock(null, null, 0), "Résultat 0 car paramètres null");
 	    }
 	    
+	    @Test
+	    void ajoutStockDepotLegalNull() {
+	        System.out.println("--------------------------- ajoutStockDepotLegalNull ---------------------------------");
+	        Assertions.assertEquals(0, unStockService.ajoutStock(null, "a131", 15), "Résultat 0 car depotLegal null");
+	    }
 	    
+	    @Test
+	    void ajoutStockMatriculeNull() {
+	        System.out.println("--------------------------- ajoutStockMatriculeNull ---------------------------------");
+	        Assertions.assertEquals(0, unStockService.ajoutStock("3MYC7", null, 15), "Résultat 0 car matricule null");
+	    }
+	    
+	    @Test
+	    void ajoutStockQteStockNull() {
+	        System.out.println("--------------------------- ajoutStockQteStockNull ---------------------------------");
+	        Assertions.assertEquals(0, unStockService.ajoutStock("3MYC7", "a131", 0), "Résultat 0 car qteStock null");
+	    }
 }
