af04a73c4b46425860d037469392a40572d1a22b
groy18 <gwendal.roy1@gmail.com>
2019-11-21 14:32:48 +0100

    Création de la classe MedicamentServiceTest

---
+++ src/gsb/tests/MedicamentServiceTest.java
@@ -0,0 +1,20 @@
+/*
+ * Créé le 21/11/2019
+ */
+
+package gsb.tests;
+import gsb.modele.Medicament;
+import gsb.service.MedicamentService;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+/*
+ * @author Gwendal
+ * 21/11/2019
+ */
+public class MedicamentServiceTest {
+	
+	
+}
