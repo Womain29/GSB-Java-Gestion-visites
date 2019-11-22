e33739002718199997c4b2b17979ccb919f68af0
Womain <romain.perennes@gmail.com>
2019-11-22 13:17:28 +0100

    Creation méthode rechercher

--- src/gsb/modele/dao/StockDao.java
+++ src/gsb/modele/dao/StockDao.java
@@ -1,11 +1,16 @@
-/*Créé le 07/11/2019 */
+/*Cr?? le 07/11/2019 */
 package gsb.modele.dao;
 /**
  * @author Gwendal
  * 7/11/2019
  * 
  */
+import gsb.modele.Medicament;
 import gsb.modele.Stock;
+import gsb.modele.Visiteur;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StockDao {
 	
@@ -26,6 +31,28 @@ public class StockDao {
         ConnexionMySql.fermerConnexionBd();
 
         return result;
+    }
+
+    public static Stock rechercher(String depotLegal, String matricule) {
+        Medicament unMedicament = null;
+        Visiteur unVisiteur = null;
+        Stock unStock = null;
+        String requete = "SELECT * FROM STOCKER WHERE DepotLegal = '" + depotLegal + "' AND Matricule = '" + matricule + "'";
+
+        ResultSet reqSelection = ConnexionMySql.execReqSelection(requete);
+
+        try {
+            if(reqSelection.next()) {
+                unMedicament = MedicamentDao.rechercher(reqSelection.getString(1));
+                unVisiteur = VisiteurDao.rechercher(reqSelection.getString(2));
+                unStock = new Stock(unMedicament, unVisiteur, reqSelection.getInt(3));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur avec la requête SELECT * FROM STOCKER WHERE DepotLegal = '" + depotLegal + "' AND Matricule = '" + matricule + "'");
+        }
+        return unStock;
     }
 	
 	
