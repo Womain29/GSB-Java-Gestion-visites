e38b76a4a8ed606254a265f62ff7cec41d3350f8
Womain <romain.perennes@gmail.com>
2019-11-28 15:49:22 +0100

    Ajout test rechercherOffrirDepotNull

--- src/gsb/tests/OffrirServiceTest.java
+++ src/gsb/tests/OffrirServiceTest.java
@@ -1,6 +1,9 @@
 package gsb.tests;
 
+import gsb.modele.Offrir;
+import gsb.service.OffrirService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,16 +11,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OffrirServiceTest {
 
+    private OffrirService uneOffreService;
+    private Offrir uneOffre;
+
     @BeforeEach
     void setUp() {
+        uneOffreService = new OffrirService();
     }
 
     @AfterEach
     void tearDown() {
+        if(uneOffreService.rechercherOffrir("3MYC7", "a131") != null) {
+            uneOffreService.supprimerOffre("3MYC7", "a131");
+        }
     }
 
     @Test
-    void rechercherOffrir() {
+    void rechercherOffrirDepotNull() {
+        System.out.println("---------------------------------- rechercherOffrirDepotNull -----------------------------------");
+        Assertions.assertNull(uneOffreService.rechercherOffrir(null, "v0001"), "Résultat null car Depot null");
     }
 
     @Test
