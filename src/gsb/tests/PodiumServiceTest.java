5ff2ed5a8745886c4dbb5e125cdf3700fa85be70
Womain <romain.perennes@gmail.com>
2020-03-23 09:02:31 +0100

    Création classe PodiumServiceTest

---
+++ src/gsb/tests/PodiumServiceTest.java
@@ -0,0 +1,24 @@
+package gsb.tests;
+
+import gsb.modele.Podium;
+import gsb.service.PodiumService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author womain
+ *
+ * Classe de tests unitaires de la classe PodiumService
+ *
+ * Créée le 23 mars 2020
+ */
+class PodiumServiceTest {
+
+
+}
