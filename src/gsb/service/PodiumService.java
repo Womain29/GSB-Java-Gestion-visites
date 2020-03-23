a87c7ef3e07b70a212b23fc5e98e4a07eb335222
Womain <romain.perennes@gmail.com>
2020-03-23 08:38:35 +0100

    Création classe PodiumService

---
+++ src/gsb/service/PodiumService.java
@@ -0,0 +1,17 @@
+package gsb.service;
+
+import gsb.modele.Podium;
+import gsb.modele.dao.PodiumDao;
+
+/**
+ * @author womain
+ *
+ * Classer service de la classe PodiumDao
+ *
+ * Créée le 23/03/2020
+ */
+
+public class PodiumService {
+
+
+}
