193c4e9f53b182b0583e6098d4b549db7408f618
Womain <romain.perennes@gmail.com>
2020-03-23 08:38:53 +0100

    Création méthode rechercherPodium

--- src/gsb/service/PodiumService.java
+++ src/gsb/service/PodiumService.java
@@ -13,5 +13,23 @@ import gsb.modele.dao.PodiumDao;
 
 public class PodiumService {
 
+    public Podium rechercherPodium(int id) {
 
+        Podium unPodium = null;
+
+        try {
+            if(id < 1) {
+                throw new Exception("L'identifiant doit être supérieur à 0");
+            }
+            if(PodiumDao.rechercher(id) == null) {
+                throw new Exception("Il n'y pas de podium correspondant à cet identifiant");
+            }
+            unPodium = PodiumDao.rechercher(id);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return unPodium;
+    }
 }
